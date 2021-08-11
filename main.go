257fdb432c633d66eafdad557493c948d37ec191
pedroalejosb <pedroalejosb@gmail.com>
2021-08-11 14:01:08 -0400

    se grega endpoint eliminar y actualizar

--- main.go
+++ main.go
@@ -10,11 +10,21 @@ import (
 	"time"
 )
 
+func getUsers( c echo.Context ) error {
+
+	users , err := user_service.Read()
+	if err != nil{
+		return c.JSON(http.StatusInternalServerError,err.Error())
+	}
+
+	return c.JSON(http.StatusOK,users)
+}
 func createUser(c echo.Context) error {
 	data := echo.Map{}
+	err := c.Bind(&data);
 
-	if err := c.Bind(&data); err != nil {
-		c.JSON(http.StatusInternalServerError,err.Error())
+	if  err != nil {
+		return c.JSON(http.StatusInternalServerError,err.Error())
 	}
 	oid := primitive.NewObjectID()
 
@@ -26,29 +36,58 @@ func createUser(c echo.Context) error {
 		UpdatedAt: time.Now(),
 	}
 
-	err := user_service.Create(user)
+	var errService error = user_service.Create(user)
 
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError,err.Error())
+	if errService != nil {
+		return c.JSON(http.StatusInternalServerError,errService.Error())
 	}
 	return c.JSON(http.StatusOK,user)
 }
+func updateUser(c echo.Context) error {
+	data := echo.Map{}
+	err := c.Bind(&data)
 
-func getUsers( c echo.Context ) error {
+	if  err != nil {
+		return c.JSON(http.StatusInternalServerError,err.Error())
+	}
+	user := models.User{
+		Name: fmt.Sprintf("%v", data["nombre"]),
+		Email: fmt.Sprintf("%v", data["correo"]),
+		UpdatedAt: time.Now(),
+	}
+	var errService error = user_service.Update(user,fmt.Sprintf("%v", data["id_user"]))
+	if errService != nil{
+		return c.JSON(http.StatusInternalServerError,errService.Error())
+	}
 
-	users , err := user_service.Read()
-	if err != nil{
+	return c.JSON(http.StatusOK,user)
+}
+func deleteUser(c echo.Context) error {
+	data := echo.Map{}
+	err := c.Bind(&data)
+
+	if  err != nil {
 		return c.JSON(http.StatusInternalServerError,err.Error())
 	}
 
-	return c.JSON(http.StatusOK,users)
+	var errService error = user_service.Delete(fmt.Sprintf("%v", data["id_user"]))
+	if errService != nil{
+		return c.JSON(http.StatusInternalServerError,errService.Error())
+	}
+
+	return c.JSON(http.StatusOK,map[string]interface{}{"mensaje":"Usuario eliminado"})
 }
+
+
+
 
 func main() {
 	e := echo.New()
 
 	e.GET("/api/user",getUsers)
 	e.POST("/api/user",createUser)
+	e.PUT("/api/user",updateUser)
+	e.DELETE("/api/user",deleteUser)
 
 
 	e.Start(":8080")
